40facf3deb1a98f6003e0eb419b50febc6a723ae
biotronik <noah.wong@biotronik.com>
2019-08-23 08:56:29 -0700

    added bio colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,39 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        crm: {
+          lime: "#e5e737",
+          green: "#00ac6c",
+          aqua: "#00afac",
+          sky: "#2183ca",
+          berry: "#860c87",
+          pink: "#bf0a64",
+          tangerine: "#f04e23",
+          blueGrey: "#587992",
+          lime2: "#4f590d",
+          green2: "#135234",
+          aqua2: "#0f5c63",
+          sky2: "#004c75",
+          berry2: "#49074b",
+          pink2: "#5f0433",
+          tangerine2: "#5a170d",
+          blueGrey2: "#00234c"
+        },
+        vi: {
+          lime1: "#b2b629",
+          lime2: "#81881b",
+          aquaDark: "#0f5c63",
+          grey: "#8e9295",
+          berry: "#720a73",
+          pink: "#d51566",
+          tangerine: "#f04e23",
+          blueGrey: "#3b5c7b",
+          aqua: "#00afac"
+        }
+      }
+    }
   },
   variants: {},
-  plugins: [],
-}
+  plugins: []
+};
